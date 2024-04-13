781ab5c2da6998e18c05f483005c13463cd3f17b
Connection <69660530+ConnectionOuOb@users.noreply.github.com>
2024-04-13 14:58:50 +0800

    v240413_1458

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(const MapScreen());
@@ -13,20 +14,33 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  GoogleMapController? mapController;
   static const _initialCameraPosition = CameraPosition(
-    target: LatLng(24.79505849588884, 120.99224060444382),
+//    target: LatLng(24.79505849588884, 120.99224060444382),
+    target: LatLng(24.7975722, 120.9799402),
     zoom: 18,
   );
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GoogleMap(
+          onMapCreated: (controller) {
+            mapController = controller;
+            _getCurrentPosition();
+          },
           initialCameraPosition: _initialCameraPosition,
         ),
       ),
     );
+  }
+
+  void _getCurrentPosition() async {
+    final Location location = Location();
+    LocationData locationData = await location.getLocation();
+    print(locationData.latitude);
+    print(locationData.longitude);
   }
 }
